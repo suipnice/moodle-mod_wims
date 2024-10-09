4350b0c5a461fe18cc10f0171a4a39dbcebd1a07
Olivier Bado-Faustin <bado@unice.fr>
2024-10-09 12:07:22 +0200

    Code formatting : ad space before Boilerplate comments

--- classes/event/course_module_viewed.php
+++ classes/event/course_module_viewed.php
@@ -13,6 +13,7 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
 /**
  * Event definition for course_module_viewed.
  *
